c2b84a5f43e0c098ae920e22d64d925027c2bea5
Eric Steadman <es3649@gmail.com>
2019-11-25 18:41:27 -0700

    created lattice specification

--- com/studmane/nlpserver/service/discourse/WordLattice.java
+++ com/studmane/nlpserver/service/discourse/WordLattice.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.logging.Level;
 
 import com.google.gson.Gson;
@@ -33,14 +34,14 @@ class WordLattice {
         String json = new String(Files.readAllBytes(file.toPath()));
 
         // prepare the deserializer
-        // Gson gson = new Gson();
-        // TypeToken<WordLattice> typTok = new TypeToken<WordLattice>() {};
+        Gson gson = new Gson();
+        TypeToken<WordLattice> typTok = new TypeToken<WordLattice>() {};
         
         // // deserialize the json into a WordLattice
-        // WordLattice wl = gson.fromJson(json, typeTok.getType());
-        WordLattice wl = new WordLattice(json);
+        WordLattice wl = gson.fromJson(json, typeTok.getType());
 
         // normalize the weights in the lattice
+        wl.assertValid();
         wl.normalize();
         return wl;
     }
@@ -56,8 +57,38 @@ class WordLattice {
      *      appropriate formatting using the given name and date
      */
     public String generate(Calendar date, String name) {
-        assert false;
-        return null;
+        // declare a string builder
+        StringBuilder sb = new StringBuilder();
+
+        // start with the root node of the lattice
+        LatticeNode cur = lattice.get(root);
+        sb.append(cur.v);
+
+        Random generator = new Random(System.currentTimeMillis());
+
+        // as long as there is a place to transition to...
+        while (!cur.to.isEmpty()) {
+            // move to the next state
+            double rand = generator.nextDouble();
+
+            // do a manual multinomial distribution (this is why things really need to be normalized)
+            for (int i = 0; i < cur.to.size(); i++) {
+                // dinishish rand by the weight
+                rand -= cur.w.get(i);
+
+                // if rand is totally diminished, then we have what we are looking for
+                if (rand <= 0) {
+                    // advance cur
+                    cur = lattice.get(cur.to.get(i));
+                }
+            }
+
+            // append the transition string
+            sb.append(cur.v);
+
+        }
+
+        return sb.toString();
     }
 
     /**
@@ -66,7 +97,41 @@ class WordLattice {
      * (up to floating point arithmetic error)
      */
     private void normalize() {
-        assert 1 == 0;
+        for (Map.Entry<String,LatticeNode> entry : this.lattice.entrySet()) {
+            double sum = 0;
+            for (double weight : entry.getValue().w) {
+                sum += weight;
+            }
+
+            if (sum == 0 && !entry.getValue().w.isEmpty()) {
+                // then the sum of weights is 0, but there need to be weights
+                // let's default to a uniform distribution
+                double weight = 1/entry.getValue().w.size();
+
+                for (int i = 0; i < entry.getValue().w.size(); i++) {
+                    entry.getValue().w.set(i, weight);
+                }
+            } else {
+                // then we need to normalize
+                for (int i = 0; i < entry.getValue().w.size(); i++) {
+                    entry.getValue().w.set(i, entry.getValue().w.get(i)/sum);
+                }
+            }
+        }
+    }
+
+    /**
+     * Throws an IOException if the word lattice is not valid
+     * 
+     * Explicitly checks that the lengths of the 'to' and 'w' members of
+     * each Lattice node are the same length.
+     */
+    private void assertValid() throws IOException {
+        for (Map.Entry<String, LatticeNode> entry : this.lattice.entrySet()) {
+            if (entry.getValue().to.size() != entry.getValue().w.size()) {
+                throw new IOException(String.format("List length mismatch on key %s", entry.getKey()));
+            }
+        }
     }
 
     /**
@@ -79,6 +144,6 @@ class WordLattice {
     class LatticeNode {
         String v;           // the string value in this node
         List<String> to;    // the nodes to which this node can transition
-        List<Float> w;      // the weights on those transitions
+        List<Double> w;      // the weights on those transitions
     }
 }
